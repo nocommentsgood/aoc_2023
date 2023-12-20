c2f533f0c5bffe45dd9b92a6b78d8debb08a29f9
Conor <conormachinedev@gmail.com>
2023-12-20 00:59:11 -0600

    Working solution to puzzle 2
    A primitive solution to puzzle 2 part 1 of AoC 2023

--- src/first_solutions/day_2.rs
+++ src/first_solutions/day_2.rs
@@ -1,1 +1,52 @@
-pub fn cube_condundrum() {}
+pub fn cube_condundrum(puzzle: &str) -> u32 {
+    const RED_CUBES: u32 = 12;
+    const GREEN_CUBES: u32 = 13;
+    const BLUE_CUBES: u32 = 14;
+    let mut sum = 0;
+
+    let sanit = puzzle.replace("Game ", "");
+    let sanit = sanit.replace("reen", "");
+    let sanit = sanit.replace("ed", "");
+    let sanit = sanit.replace("lue", "");
+    let sanit = sanit.replace(" ", "");
+    let sanit = sanit.replace(";", ",");
+
+    let v: Vec<&str> = sanit.lines().collect();
+
+    for i in 0..v.len() {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+        let game_index = v[i].find(":");
+
+        if game_index.is_some() {
+            let (id, puz) = v[i].split_at(game_index.unwrap());
+            let puz = puz.replace(":", "");
+
+            'rep: for cube in puz.split(',') {
+                let (num, color) = cube.split_at(cube.len() - 1);
+
+                if let Ok(num) = num.parse::<u32>() {
+                    match color {
+                        "r" => red = num,
+                        "g" => green = num,
+                        "b" => blue = num,
+                        _ => eprintln!("Bad color parsing: {}", color),
+                    }
+                }
+
+                if !(red <= RED_CUBES && blue <= BLUE_CUBES && green <= GREEN_CUBES) {
+                    break 'rep;
+                }
+            }
+
+            if red <= RED_CUBES && blue <= BLUE_CUBES && green <= GREEN_CUBES {
+                if let Ok(n) = id.parse::<u32>() {
+                    sum += n;
+                }
+            }
+        }
+    }
+
+    sum
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use std::fs;
 pub mod first_solutions;
 
 fn main() {
-    let trebuchet_puzzle =
-        fs::read_to_string("/home/clushh/rust/aoc_2023/src/puzzles/trebuchet_puzzle.txt").unwrap();
-    println!("{}", trebuchet(trebuchet_puzzle.as_str()));
+    // let trebuchet_puzzle =
+    //     fs::read_to_string("/home/clushh/rust/aoc_2023/src/puzzles/trebuchet_puzzle.txt").unwrap();
+    // println!("{}", trebuchet(trebuchet_puzzle.as_str()));
+
+    let cube_puzzle = fs::read_to_string("/home/clushh/rust/aoc_2023/src/puzzles/cube_conundrum_puzzle.txt").unwrap();
+    print!("{}", cube_condundrum(cube_puzzle.as_str()));
 }
